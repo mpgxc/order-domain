540e95a54e0996acad5c1601f704194c2916667f
mpgxc <mpgx5.c@gmail.com>
2022-04-27 00:08:49 -0300

    feat: added domain entities

---
+++ src/domain/customers/customer.ts
@@ -0,0 +1,21 @@
+import { AggregateRoot } from '../../common/aggregate-root';
+
+type CustomerProps = {
+  name: string;
+};
+
+class Customer extends AggregateRoot<CustomerProps> {
+  get name() {
+    return this._props.name;
+  }
+
+  private constructor(props: CustomerProps, id?: string) {
+    super(props, id);
+  }
+
+  static build(props: CustomerProps, id?: string) {
+    return new this(props, id);
+  }
+}
+
+export { Customer };
---
+++ src/domain/products/product.ts
@@ -0,0 +1,26 @@
+import { AggregateRoot } from '../../common/aggregate-root';
+
+type ProductProps = {
+  name: string;
+  price: number;
+};
+
+class Product extends AggregateRoot<ProductProps> {
+  get price() {
+    return this._props.price;
+  }
+
+  get name() {
+    return this._props.name;
+  }
+
+  private constructor(props: ProductProps, id?: string) {
+    super(props, id);
+  }
+
+  static build(props: ProductProps, id?: string) {
+    return new this(props, id);
+  }
+}
+
+export { Product };
