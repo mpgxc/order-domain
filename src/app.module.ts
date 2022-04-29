ce40199f67d3bdc6b4edbfe785ff38172b324661
mpgxc <mpgx5.c@gmail.com>
2022-04-28 23:52:35 -0300

    feat: resolve dip

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { CreateCustomerInteractor } from 'app/interactors/create-customer.interactor';
+import { CustomersRepository } from './app/repositories/customers.repository';
+import { InfraModule } from 'infra/infra.module';
 import { AppController } from './app.controller';
-import { CreatePurchaseInteractor } from './app/create-purchase.interactor';
+import { CreatePurchaseInteractor } from './app/interactors/create-purchase.interactor';
+import { CreateProductInteractor } from './app/interactors/create-product.interactor';
+import { ProductsRepository } from './app/repositories/products.repository';
+import { PurchaseRepository } from './app/repositories/purchases.repository';
+import { PurchaseMixinRepository } from './app/repositories/mixin.repository';
 
 @Module({
-  imports: [],
+  imports: [InfraModule],
   controllers: [AppController],
-  providers: [CreatePurchaseInteractor],
+  providers: [
+    PurchaseMixinRepository,
+    //Products
+    CreateProductInteractor,
+    ProductsRepository,
+
+    //Purchases
+    CreatePurchaseInteractor,
+    PurchaseRepository,
+
+    //Customers
+    CreateCustomerInteractor,
+    CustomersRepository,
+  ],
 })
 export class AppModule {}
