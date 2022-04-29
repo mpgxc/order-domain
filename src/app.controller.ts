ae4b646e0d1b346eb305fa9233237bebd23e60fa
mpgxc <mpgx5.c@gmail.com>
2022-04-28 23:53:04 -0300

    feat: added controllers to interactors

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,39 @@
-import { Body, Controller, Get } from '@nestjs/common';
-import { CreatePurchaseInput } from 'app/create-purchase.input';
-import { CreatePurchaseInteractor } from './app/create-purchase.interactor';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreatePurchaseInput } from './inputs/create-purchase.input';
+import { CreateCustomerInteractor } from './app/interactors/create-customer.interactor';
+import { CreatePurchaseInteractor } from './app/interactors/create-purchase.interactor';
+import { CreateCustomerInput } from './inputs/create-customer.input';
+import { CreateProductInput } from './inputs/create-product.input';
+import { CreateProductInteractor } from './app/interactors/create-product.interactor';
 
-@Controller('purchase')
+@Controller()
 export class AppController {
-  constructor(private readonly interactor: CreatePurchaseInteractor) {}
+  constructor(
+    private readonly purchaseInteractor: CreatePurchaseInteractor,
+    private readonly customerInteractor: CreateCustomerInteractor,
+    private readonly productInteractor: CreateProductInteractor,
+  ) {}
 
-  @Get()
-  async createOrder(@Body() props: CreatePurchaseInput): Promise<void> {
-    console.log(props);
+  @Get('purchase')
+  async createPurchase(@Body() props: CreatePurchaseInput): Promise<void> {
+    return this.purchaseInteractor.handle({
+      customerId: props.customerId,
+      products: props.products,
+    });
+  }
 
-    return this.interactor.handle();
+  @Post('customer')
+  async createCustomer(@Body() props: CreateCustomerInput): Promise<void> {
+    return this.customerInteractor.handle({
+      name: props.name,
+    });
+  }
+
+  @Post('product')
+  async createProduct(@Body() props: CreateProductInput): Promise<void> {
+    return this.productInteractor.handle({
+      name: props.name,
+      price: props.price,
+    });
   }
 }
